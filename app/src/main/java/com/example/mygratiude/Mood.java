ddc23a6078066f498b6934c27908394ca6230bc3
Nahal Golestanifard <a12025425@unet.univie.ac.at>
2023-11-09 20:49:32 +0100

    finalized the Mood Enum Class

--- app/src/main/java/com/example/mygratiude/Mood.java
+++ app/src/main/java/com/example/mygratiude/Mood.java
@@ -1,5 +1,80 @@
 package com.example.mygratiude;
 
+import java.util.logging.Logger;
+
 public enum Mood {
-    RELAXED("relaxed")
+
+    RELAXED("relaxed"),
+    NEUTRAL ("neutral"),
+    STRESSED ("stressed"),
+    HAPPY("happy"),
+    SAD("sad"),
+    CONFUSED("confused");
+    private String description;
+    private static final Logger logger = Logger.getLogger(Mood.class.getName());
+    private Mood(String description) {
+        this.description = description;
+    }
+
+    public Mood getEnumValueOf(String description){
+        Mood mood = null;
+        switch(description){
+            case "relaxed":{
+                mood= RELAXED;
+                break;
+            }
+            case "neutral":{
+                mood = NEUTRAL;
+                break;
+            }
+            case "happy":{
+                mood = HAPPY;
+                break;
+            }
+            case "sad":{
+                mood =  SAD;
+                break;
+            }
+            case "confused":{
+                mood = CONFUSED;
+                break;
+            }
+            default:{
+                logger.warning("No such enum found: " + description);
+                break;
+            }
+        }
+        return mood;
+    }
+
+    public String getStringOfMood(Mood mood){
+        String description = null;
+        switch (mood){
+            case RELAXED:{
+                description = "relaxed";
+                break;
+            }
+            case NEUTRAL:{
+                description = "neutral";
+                break;
+            }
+            case HAPPY:{
+                description="happy";
+                break;
+            }
+            case SAD:{
+                description="sad";
+                break;
+            }
+            case CONFUSED:{
+                description = "confused";
+                break;
+            }
+            default:{
+                logger.warning("No such enum found: " + description);
+                break;
+            }
+        }
+        return description;
+    }
 }
